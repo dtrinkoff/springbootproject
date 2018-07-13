b0004c08fd0cd40278ebb47ea011fce3545670ed
David Trinkoff <dtrinkoff@catalyte.io>
2018-07-13 11:11:50 -0400

    got the full version

---
+++ src/test/java/com/catalyte/springboot/controllers/HomeControllerTest.java
@@ -0,0 +1,12 @@
+package com.catalyte.springboot.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HomeControllerTest {
+    @Test
+    public void FakeTest(){
+        assertTrue(true);
+    }
+}
